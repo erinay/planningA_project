c1008359bcbd650767bf2b56cd56bf9851d8e898
eyamagu2@caltech.edu <erinay@mit.edu>
2025-03-18 11:45:41 -0700

    finished a_star

--- a_star.py
+++ a_star.py
@@ -1,6 +1,7 @@
-import heapq
 import numpy as np
-
+import matplotlib.pyplot as plt
+from matplotlib import colormaps
+from queue import PriorityQueue
 
 class Node():
     def __init__(self, parent=None, pos=None):
@@ -10,8 +11,16 @@ class Node():
         self.h = 0
         self.f = 0
 
+    def __eq__(self, other):
+        return self.pos==other.pos
+
+    def __lt__(self,other):
+        return self.f < other.f
+
 def heuristic(a,b):
-    distance = np.linalg.norm(a-b)
+    a_np = np.array(a)
+    b_np = np.array(b)
+    distance = np.linalg.norm(a_np-b_np)
     return distance
 
 def a_star(start, goal, grid):
@@ -20,40 +29,48 @@ def a_star(start, goal, grid):
     goal_node = Node(None, goal)
     path = []
 
+    x_max = grid.shape[0]-1
+    y_max = grid.shape[1]-1
+
     # queue
-    queue = [start_node]
+    queue = PriorityQueue()
+    queue.put(start_node)
     visited = []
 
     #list to check neighbors:
     nbhrs = [(0,1), (0,-1), (1,0), (-1,0), (1,1), (1,-1), (-1,1), (-1,-1)]
 
-    while queue:
-        # use f to pick "best" partial path N from queue
-        N = 0
-        
+    while not queue.empty():
+        curr_node = queue.get()
+
         # If node = goal, return oath
         if(curr_node.pos==goal_node.pos):
             child = curr_node
             while child:
                 path.append(child.pos)
                 child = child.parent
-            return path[::-1] 
-        
-        # Else remove N from queue
-        curr_node = queue.pop(N)
+            return path[::-1]
 
         # If N is in visited, go back to beginning
-        if N in visited:
+        if curr_node in visited:
             pass
-        
-        # Else find children and add to queue
-        else: 
+        # Else find all children NOT in expanded and is a a valid grid location
+        else:
+            visited.append(curr_node)
             pos = curr_node.pos
             for nbhr in nbhrs:
                 child_pos = (pos[0]+nbhr[0], pos[1]+nbhr[1])
-                
-
-
-
+                child_node = Node(curr_node, child_pos)
+                if child_node in visited:
+                    pass
+                elif child_pos[0] < 0 or child_pos[0] > x_max or child_pos[1]<0 or child_pos[1]>y_max:
+                    pass
+                elif grid[child_pos[0]][child_pos[1]] == 1 :
+                    pass
+                else:
+                    child_node.g = curr_node.g+1
+                    child_node.h = heuristic(child_pos, goal)
+                    child_node.f = child_node.g+child_node.h
+                    queue.put(child_node)
+    print('failure')
     return path
-
--- visualize_grid.py
+++ visualize_grid.py
@@ -1,14 +1,14 @@
 import numpy as np
 import matplotlib.pyplot as plt
-from a_star import astar
+from a_star import a_star
 
 def plot_grid(grid, start, goal, path=None):
-    plt.imshow(grid)
+    plt.imshow(grid, cmap='YlGn')
 
     # Plot start and goal
     plt.scatter(start[1], start[0], marker="o", color="green", label="Start")
     plt.scatter(goal[1], goal[0], marker="x", color="red", label="Goal")
-    
+
     # Plot path
     if path:
         for (x, y) in path:
@@ -17,18 +17,17 @@ def plot_grid(grid, start, goal, path=None):
     plt.legend()
     plt.grid(True, which='both', color='gray', linestyle='-', linewidth=0.5)
     plt.show()
-    
+
 
 N = 20
-gridsize = (N,N)
+gridsize = (N,N+10)
 grid = np.zeros(gridsize)
 
 ## Add obstacles
 grid[3, 3:7] = 1
 
-start = (1,1)
-goal = (14, 17)
+start = (N-1,N+10-1)
+goal = (3, 8)
 
-# path = a_star(grid, start, goal)
-plot_grid(grid, start, goal, path=None)
-
+path = a_star(start, goal, grid)
+plot_grid(grid, start, goal, path)
